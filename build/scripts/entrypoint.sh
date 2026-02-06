804b6a95b67c8af00c8abdcfda28d100adf44a55
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-02-05 19:09:39 -0800

    feat: added ability to set jwt secret
    Signed-off-by: Hendrik Leidinger <hendrik.leidinger@nextcloud.com>

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -9,4 +9,28 @@ service nginx start
 /usr/bin/documentserver-generate-allfonts.sh
 
 
+
+CONFIG_FILE="$EO_CONF/local.json"
+
+jq_filter='.'
+
+[ -n "$JWT_SECRET" ] && \
+  jq_filter="$jq_filter | .services.CoAuthoring.secret.browser.string = \$jwtSecret"
+  jq_filter="$jq_filter | .services.CoAuthoring.secret.inbox.string   = \$jwtSecret"
+  jq_filter="$jq_filter | .services.CoAuthoring.secret.outbox.string  = \$jwtSecret"
+  jq_filter="$jq_filter | .services.CoAuthoring.secret.session.string = \$jwtSecret"
+
+[ -n "$DB_PASSWORD" ] && \
+  jq_filter="$jq_filter | .services.CoAuthoring.sql.dbPass = \$dbPassword"
+
+if [ "$jq_filter" != "." ]; then
+  jq \
+    --arg jwtSecret "$JWT_SECRET" \
+    --arg dbPassword "$DB_PASSWORD" \
+    "$jq_filter" \
+    "$CONFIG_FILE" > "${CONFIG_FILE}.tmp"
+
+  mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+fi
+
 /usr/bin/supervisord
